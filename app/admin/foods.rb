11fefa05e61d6022037f3047fd6a1f670d9805a6
Aarakka12 <aarakka@gammastack.com>
2023-03-31 18:20:22 +0530

    changes in active admin

--- app/admin/foods.rb
+++ app/admin/foods.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Food do
   
-  permit_params :name, :description, :price, :status, :category_id, :prep_time
+  permit_params [:name, :description, :price, :image, :category_id, :prep_time]
 
 end
