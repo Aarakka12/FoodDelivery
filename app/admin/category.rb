40d04c31b77f9d41e0978705883c9ee83f1ce282
Aarakka12 <aarakka@gammastack.com>
2023-03-31 18:20:22 +0530

    changes in active admin

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Category do
   permit_params :title, :description
 
   action_item :foods, only: [:create, :show] do
-    link_to 'Add Foods', "category_path(category)"
+    link_to 'Add Foods', "new_admin_food_path(category)"
   end
 
 end
